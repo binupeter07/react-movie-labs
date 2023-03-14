34fd14a726f1d823c9c78f7003ede25f398b9a46
Binu Peter <binupeterp07@gmail.com>
2023-03-14 10:17:29 +0000

    Added new review support.

--- movies/src/index.js
+++ movies/src/index.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect}  from "react";
 import { useParams } from 'react-router-dom';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
-
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
@@ -40,6 +40,7 @@ const App = () => {
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
+          <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
         </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
