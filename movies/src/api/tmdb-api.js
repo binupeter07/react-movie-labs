9707d3e65426af1afee6dd35a8524eb52e3861fe
Binu Peter <binupeterp07@gmail.com>
2023-03-19 20:58:55 +0000

    Fix upcoming movies page.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -76,3 +76,18 @@ export const getMovieReviews = (id) => {
       return json.results;
     });
 };
+
+
+export const upcomingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
--- movies/src/index.js
+++ movies/src/index.js
@@ -8,7 +8,7 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
-import UpcomingMoviePage from "./pages/upcomingMoviePage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
@@ -35,7 +35,7 @@ const App = () => {
         <MoviesContextProvider>
         <Routes>
           <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-          {/* <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} /> */}
+          { <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />}
           <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/" element={<HomePage />} />
